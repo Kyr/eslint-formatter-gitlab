23bf448c82a4c5faa4b1c12d069560eb7d7e1dd0
Kyrylo Fedorov <kyrylofedorov@gmail.com>
2019-04-21 16:16:38 +0300

    Near done approach

---
+++ .typedef/Result.js
@@ -0,0 +1,9 @@
+/**
+ * @typedef {Object} Result
+ * @property {string} filePath
+ * @property {Message[]} messages
+ * @property {number} errorCount
+ * @property {number} warningCount
+ * @property {string} [source]
+ * @property {string} [output]
+ */
