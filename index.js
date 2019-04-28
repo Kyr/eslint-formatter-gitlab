213b734b8d68302534811fe217a56a0eeca302ad
Kyrylo Fedorov <426462+Kyr@users.noreply.github.com>
2019-04-28 16:26:38 +0300

    Feature: make formatted report more readable in context GitLab UI (#2)

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const xml = require('./lib/xml');
 const formatResult = require('./lib/formatResult');
 const reduceToString = require('./lib/fns/reduceToString');
 const resultsFormatter = reduceToString(formatResult);
-const testSuites = xml.createNodeConstructor('testsuites');
 
 module.exports = xmlTemplate;
 
@@ -13,5 +12,17 @@ module.exports = xmlTemplate;
  */
 function xmlTemplate (results) {
 	const formattedResults = resultsFormatter(results);
-	return '<?xml version="1.0" encoding="utf-8"?>' + testSuites({}, formattedResults);
+	const testSuite = xml.createNode('testsuite', {
+		id:       'org.eslint',
+		name:     'Eslint',
+		time:     0,
+		tests:    1,
+		failures: results.length,
+	}, formattedResults);
+	const testSuites = xml.createNode('testsuites', {
+		package: 'org.eslint',
+		tests: 1,
+		failures: results.length
+	}, testSuite);
+	return '<?xml version="1.0" encoding="utf-8"?>' + testSuites;
 }
--- test/index.js
+++ test/index.js
@@ -1,67 +1,153 @@
-// based on https://github.com/eslint/eslint/blob/master/tests/lib/formatters/junit.js
 const test = require('tape');
 const formatter = require('../index');
 
 test('when there are no problems', noProblem);
 
-test('when passed a single message', singleMessage);
+test('format report', formatReport);
 
-test('when passed multiple messages', multipleMessages);
-
-function multipleMessages (a) {
-	a.plan(1);
+function formatReport (test) {
+	test.plan(1);
 	const payload = [
 		{
-			filePath: 'foo.js',
-			messages: [
+			'filePath':            'fixtures/app/fullOfProblem.js',
+			'messages':            [
 				{
-					message:  'Unexpected foo.',
-					severity: 2,
-					line:     5,
-					column:   10,
-					ruleId:   'foo',
-				}, {
-					message:  'Unexpected bar.',
-					severity: 1,
-					line:     6,
-					column:   11,
-					ruleId:   'bar',
+					'ruleId':    'no-unused-vars',
+					'severity':  2,
+					'message':   '\'addOne\' is defined but never used.',
+					'line':      1,
+					'column':    10,
+					'nodeType':  'Identifier',
+					'endLine':   1,
+					'endColumn': 16,
+				},
+				{
+					'ruleId':    'use-isnan',
+					'severity':  2,
+					'message':   'Use the isNaN function to compare with NaN.',
+					'line':      2,
+					'column':    6,
+					'nodeType':  'BinaryExpression',
+					'messageId': 'useIsNaN',
+					'endLine':   2,
+					'endColumn': 14,
+				},
+				{
+					'ruleId':   'semi',
+					'severity': 2,
+					'message':  'Missing semicolon.',
+					'line':     3,
+					'column':   14,
+					'nodeType': 'ReturnStatement',
+					'fix':      {
+						'range': [
+							51,
+							51,
+						],
+						'text':  ';',
+					},
+				},
+				{
+					'ruleId':   'semi',
+					'severity': 2,
+					'message':  'Missing semicolon.',
+					'line':     5,
+					'column':   9,
+					'nodeType': 'ReturnStatement',
+					'fix':      {
+						'range': [
+							70,
+							70,
+						],
+						'text':  ';',
+					},
+				},
+				{
+					'ruleId':    'no-extra-semi',
+					'severity':  2,
+					'message':   'Unnecessary semicolon.',
+					'line':      7,
+					'column':    2,
+					'nodeType':  'EmptyStatement',
+					'messageId': 'unexpected',
+					'endLine':   7,
+					'endColumn': 3,
+					'fix':       {
+						'range': [
+							74,
+							76,
+						],
+						'text':  '}',
+					},
 				},
 			],
+			'errorCount':          5,
+			'warningCount':        0,
+			'fixableErrorCount':   3,
+			'fixableWarningCount': 0,
+			'source':              'function addOne(i) {\n\tif (i != NaN) {\n\t\treturn i ++\n\t} else {\n\t\treturn\n\t}\n};\n',
+		},
+		{
+			'filePath':            'fixtures/app/tooComplex.js',
+			'messages':            [
+				{
+					'ruleId':    'no-unused-vars',
+					'severity':  1,
+					'message':   '\'toComplex\' is defined but never used.',
+					'line':      1,
+					'column':    10,
+					'nodeType':  'Identifier',
+					'endLine':   1,
+					'endColumn': 19,
+				},
+				{
+					'ruleId':    'no-constant-condition',
+					'severity':  2,
+					'message':   'Unexpected constant condition.',
+					'line':      2,
+					'column':    5,
+					'nodeType':  'BinaryExpression',
+					'messageId': 'unexpected',
+					'endLine':   2,
+					'endColumn': 10,
+				},
+			],
+			'errorCount':          1,
+			'warningCount':        1,
+			'fixableErrorCount':   2,
+			'fixableWarningCount': 0,
+			'source':              'function toComplex(){\n\tif(2 > 1) {\n\t\tif ( 2 >1){\n\t\t\tif ( 2 >1){\n\t\t\t\t;\n\t\t\t} else {\n\t\t\t\t;\n\t\t\t}\n\t\t\t;\n\t\t} else {\n\t\t\tif ( 2 >1){\n\t\t\t\t;\n\t\t\t} else {\n\t\t\t\t;\n\t\t\t}\n\t\t\t;\n\t\t}\n\n\t}else {\n\t\tif (2 > 1) {\n\t\t\tif ( 2 >1){\n\t\t\t\t;\n\t\t\t} else {\n\t\t\t\t;\n\t\t\t}\n\n\t\t}else {\n\t\t\tif ( 2 >1){\n\t\t\t\t;\n\t\t\t} else {\n\t\t\t\t;\n\t\t\t}\n\t\t}\n\t}\n}\n',
+		},
+		{
+			'filePath':            'fixtures/app/fatal.js',
+			'messages':            [
+				{
+					'ruleId':   null,
+					'fatal':    true,
+					'severity': 2,
+					'message':  'Fatal error, whatever',
+				},
+			],
+			'errorCount':          1,
+			'warningCount':        0,
+			'fixableErrorCount':   0,
+			'fixableWarningCount': 0,
+			'source':              'import path from \'path\';\nimport path from \'path\';\n',
 		},
 	];
-	const actual = formatter(payload).replace(/\n/gu, '');
-	const expected = '<?xml version="1.0" encoding="utf-8"?><testsuites><testsuite package="org.eslint" time="0" tests="2" errors="2" name="foo.js"><testcase time="0" name="org.eslint.foo" classname="foo.js"><failure type="failure" message="Unexpected foo."><![CDATA[Error at foo.js:5:10 - Unexpected foo. (foo)]]></failure></testcase><testcase time="0" name="org.eslint.bar" classname="foo.js"><failure type="failure" message="Unexpected bar."><![CDATA[Warning at foo.js:6:11 - Unexpected bar. (bar)]]></failure></testcase></testsuite></testsuites>';
+	//language=XML
+	const expected = '<?xml version="1.0" encoding="utf-8"?><testsuites package="org.eslint" tests="1" failures="3"><testsuite id="org.eslint" name="Eslint" time="0" tests="1" failures="3"><testcase time="0" classname="fixtures/app/fullOfProblem.js" id="fixtures/app/fullOfProblem.js" name="fixtures/app/fullOfProblem.js - 5 issue(s)"><failure type="error" message="Eslint report: fixtures/app/fullOfProblem.js"><![CDATA[Error at fixtures/app/fullOfProblem.js:1:10 - \'addOne\' is defined but never used. (no-unused-vars)\nError at fixtures/app/fullOfProblem.js:2:6 - Use the isNaN function to compare with NaN. (use-isnan)\nError at fixtures/app/fullOfProblem.js:3:14 - Missing semicolon. (semi)\nError at fixtures/app/fullOfProblem.js:5:9 - Missing semicolon. (semi)\nError at fixtures/app/fullOfProblem.js:7:2 - Unnecessary semicolon. (no-extra-semi)]]></failure></testcase><testcase time="0" classname="fixtures/app/tooComplex.js" id="fixtures/app/tooComplex.js" name="fixtures/app/tooComplex.js - 2 issue(s)"><failure type="error" message="Eslint report: fixtures/app/tooComplex.js"><![CDATA[Warning at fixtures/app/tooComplex.js:1:10 - \'toComplex\' is defined but never used. (no-unused-vars)\nError at fixtures/app/tooComplex.js:2:5 - Unexpected constant condition. (no-constant-condition)]]></failure></testcase><testcase time="0" classname="fixtures/app/fatal.js" id="fixtures/app/fatal.js" name="fixtures/app/fatal.js - 1 issue(s)"><failure type="error" message="Eslint report: fixtures/app/fatal.js"><![CDATA[Error at fixtures/app/fatal.js - Fatal error, whatever]]></failure></testcase></testsuite></testsuites>';
+	const actual = formatter(payload);
 
-	a.equal(actual, expected);
+	test.equal(actual, expected);
 }
 
-function noProblem (t) {
-	t.plan(1);
+
+function noProblem (test) {
+	test.plan(1);
 	const code = [];
-	const result = formatter(code);
-	t.equal(result.replace(/\n/gu, ''), '<?xml version="1.0" encoding="utf-8"?><testsuites></testsuites>');
-}
-
-function singleMessage (t) {
-	t.plan(1);
-	const payload = [
-		{
-			filePath: 'foo.js',
-			messages: [
-				{
-					message:  'Unexpected foo.',
-					severity: 2,
-					line:     5,
-					column:   10,
-					ruleId:   'foo',
-				},
-			],
-		},
-	];
-
-	const result = formatter(payload).replace(/\n/gu, '');
-	const expected = '<?xml version="1.0" encoding="utf-8"?><testsuites><testsuite package="org.eslint" time="0" tests="1" errors="1" name="foo.js"><testcase time="0" name="org.eslint.foo" classname="foo.js"><failure type="failure" message="Unexpected foo."><![CDATA[Error at foo.js:5:10 - Unexpected foo. (foo)]]></failure></testcase></testsuite></testsuites>';
-
-	t.equal(result, expected);
+	//language=XML
+	const expected = '<?xml version="1.0" encoding="utf-8"?><testsuites package="org.eslint" tests="1" failures="0"><testsuite id="org.eslint" name="Eslint" time="0" tests="1" failures="0"></testsuite></testsuites>';
+	const actual = formatter(code);
+	test.equal(actual, expected);
 }
